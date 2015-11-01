60f11ba89fb8153f49ffc0d2e8ea0a5dfaa607e9
Yengas <yigitcan@hotmail.com.tr>
2015-11-01 12:27:46 -0500

    Small fix

--- initializer.sh
+++ initializer.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
-sudo apt-get install git deluge deluge-web lighttpd
+sudo apt-get install deluge deluged deluge-web lighttpd
 git clone https://github.com/Yengas/torrenter.git .
 mkdir Downloads
+deluged
 lighttpd -D -f lighttpd.conf&
 deluge-web -p 7373&
