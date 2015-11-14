2ee8fc416304c8e3effd5b519cdfc39ce5f2c84a
Yengas <yigitcan@hotmail.com.tr>
2015-11-14 07:18:11 -0500

    Fixed lighttpd run

--- initializer.sh
+++ initializer.sh
@@ -3,5 +3,6 @@ sudo apt-get install deluge deluged deluge-web lighttpd
 git clone https://github.com/Yengas/torrenter.git .
 mkdir Downloads
 deluged
+pkill lighttpd
 lighttpd -D -f lighttpd.conf&
 deluge-web -p 7373&
